186263335239c48f71ccc6b3bdaba12df4d7834b
Nicholas McNamara <nikomcnamara@gmail.com>
2024-05-30 22:06:44 -0400

    updated oauth config but still not working in react

--- src/main/java/com/tufas/project/tufasgo/Security/SecurityConfiguration.java
+++ src/main/java/com/tufas/project/tufasgo/Security/SecurityConfiguration.java
@@ -1,6 +1,7 @@
 package com.tufas.project.tufasgo.Security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -17,6 +18,11 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -25,6 +31,11 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration  {
+
+    @Value("${spring.security.oauth2.client.registration.google.client-id}")
+    private String clientId;
+    @Value("${spring.security.oauth2.client.registration.google.client-secret}")
+    private String clientSecret;
 
     @Autowired
     private UserDetailsService userDetailsService;
@@ -65,5 +76,22 @@ public class SecurityConfiguration  {
 
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.authenticationProvider(authenticationProvider());
+    }
+
+    @Bean
+    public ClientRegistrationRepository clientRegistrationRepository() {
+        return new InMemoryClientRegistrationRepository(
+                ClientRegistration.withRegistrationId("google")
+                        .clientId(clientId)
+                        .clientSecret(clientSecret)
+                        .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+                        .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+                        .authorizationUri("https://accounts.google.com/o/oauth2/v2/auth")
+                        .tokenUri("https://www.googleapis.com/oauth2/v4/token")
+                        .redirectUri("{baseUrl}/login/oauth2/code/google")
+                        .jwkSetUri("http://localhost:3000/")
+                        .scope("openid", "email", "profile")
+                        .build()
+        );
     }
 }
