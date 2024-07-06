396854ff628fb4ecc5f86f5c7b6d4a6be8182421
Nicholas McNamara <nikomcnamara@gmail.com>
2024-07-05 23:39:42 -0400

    modifying climb to include area, areaName, climbDesc, and typegrade, added area to join table because there's no way to filter openbeta to search by climb id, will have to search by area, then search for a match on id to get climb properties

--- src/main/java/com/tufas/project/tufasgo/Entities/Climb.java
+++ src/main/java/com/tufas/project/tufasgo/Entities/Climb.java
@@ -10,20 +10,28 @@ import java.util.List;
 public class Climb {
     @Id
     private String climbId;
+    private String area;
+    private String areaName;
     private String climbName;
     private String climbType;
+    private String climbDescription;
+    private String gradeType;
     private String climbGrade;
     private Double latitude;
     private Double longitude;
     @ManyToMany(cascade = CascadeType.ALL)
-    @JoinTable(name = "climbs_by_users", joinColumns = @JoinColumn(name="climbId", referencedColumnName = "climbId"),
+    @JoinTable(name = "climbs_by_users", joinColumns = {@JoinColumn(name = "climbId", referencedColumnName = "climbId"), @JoinColumn(name = "area", referencedColumnName = "area")},
     inverseJoinColumns = @JoinColumn(name = "userId", referencedColumnName = "userId"))
     private List<User> climbLog;
 
-    public Climb(String climbId, String climbName, String climbType, String climbGrade, Double latitude, Double longitude, List<User> climbLog) {
+    public Climb(String climbId, String area, String areaName, String climbName, String climbType, String climbDescription, String gradeType, String climbGrade, Double latitude, Double longitude, List<User> climbLog) {
         this.climbId = climbId;
+        this.area = area;
+        this.areaName = areaName;
         this.climbName = climbName;
         this.climbType = climbType;
+        this.climbDescription = climbDescription;
+        this.gradeType = gradeType;
         this.climbGrade = climbGrade;
         this.latitude = latitude;
         this.longitude = longitude;
@@ -50,6 +58,22 @@ public class Climb {
         this.climbId = climbId;
     }
 
+    public String getArea() {
+        return area;
+    }
+
+    public void setArea(String area) {
+        this.area = area;
+    }
+
+    public String getAreaName() {
+        return areaName;
+    }
+
+    public void setAreaName(String areaName) {
+        this.areaName = areaName;
+    }
+
     public String getClimbName() {
         return climbName;
     }
@@ -64,6 +88,22 @@ public class Climb {
 
     public void setClimbType(String climbType) {
         this.climbType = climbType;
+    }
+
+    public String getClimbDescription() {
+        return climbDescription;
+    }
+
+    public void setClimbDescription(String climbDescription) {
+        this.climbDescription = climbDescription;
+    }
+
+    public String getGradeType() {
+        return gradeType;
+    }
+
+    public void setGradeType(String gradeType) {
+        this.gradeType = gradeType;
     }
 
     public String getClimbGrade() {
