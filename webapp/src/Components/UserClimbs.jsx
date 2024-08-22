14a1ca35630b2e89b0aa2c0c444064326b15620c
Nicholas McNamara <nikomcnamara@gmail.com>
2024-08-22 15:37:11 -0400

    climb data showing as grid for userClimbs but buggy. need to ensure data is loaded prior to rendering component and display something while fetchClimbs is running

--- webapp/src/Components/UserClimbs.jsx
+++ webapp/src/Components/UserClimbs.jsx
@@ -2,17 +2,28 @@ import { useEffect, useState } from "react";
 import GetRequestWithToken from "./APICalls/GetRequestWithToken";
 
 const UserClimbs = () => {
-    const [currentUser, setCurrentUser] = useState('');
-    const [climbData, setClimbData] = useState({});
+    const [climbData, setClimbData] = useState();
     let groupedData;
 
-    useEffect( async () => {
+    useEffect(() => {
         let userData = JSON.parse(sessionStorage.getItem('user'));
-        setCurrentUser(userData.userId);
-        
-        const userClimbs = await GetRequestWithToken('GET', `http://localhost:8080/api/climblog/${currentUser}`);
-        setClimbData(userClimbs);
-        console.log(currentUser);
+        let currentUser = userData.userId;
+        const fetchUserClimbs = async () => {
+            if (userData) {
+                try {
+                    const userClimbs = await GetRequestWithToken('GET', `http://localhost:8080/api/climblog/${currentUser}`);
+                    setClimbData(userClimbs);
+                }
+                catch (error) {
+                    console.error("Error fetching data:", error);
+                }
+            }
+            else {
+                console.error("Current user not found in session storage.");
+            }
+        }
+
+        fetchUserClimbs();
     }, []);
 
 
@@ -20,13 +31,15 @@ const UserClimbs = () => {
 
     return (
         <div className="myclimbs-container">
-            {climbData.map((climb, index) => (
+            {/* {climbData.map((climb, index) => (
                 <div className="climb-container">
                     <h2 className="climb-area-name">{climb.area}</h2>
                     <h3 className="climb-name">{climb.climb.climbName}</h3>
                     <img src="https://maps.googleapis.com/maps/api/staticmap?center=40.714728,-73.998672" alt="" className="map" />
+                    
                 </div>
-            ))}
+            ))} */}
+            {console.log(climbData)}
         </div>
     )
 }
